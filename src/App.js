b72f75867e24581e0b8d2f12ffe8d5ce2485131b
Olena911 <olenaazulay@gmail.com>
2023-04-27 11:16:32 +0300

    added Forecast and Date page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
+import React, { useState } from "react";
 
 import './App.css';
+import axios from "axios";
+
+
+
+const [city, setCity] = useState("");
+const [weather, setWeather] = useState({});
 
 function App() {
-  var weatherData = {
-    city: "New York",
-    temperature: 19,
-    date: "Tuesday 10:00",
-    description: "Cloudy",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-    precipitation: 0,
-    humidity: 80,
-    wind: 10
-  };
+  
+function displayWeather (response){
+  setWeather({ temperature:response.data.main.temp,
+    wind: response.data.wind.speed,
+    humidity:response.data.main.humidity,
+    icon:`http://openweathermap.org/img/wn${response.data.weather[0].icon}@2x.png`,
+    precipitation:response.data.minutely.precipitation,
+  })
+}
+function formatDate(date){
+
+  var date = now.getDate();
+  
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    let day=days[now.getDay()];
+    let hours = now.getHours();
+    if(hours<10){ hours`${hours}`;}
+    let minutes=now.getMinutes();
+    if (minutes<10){minutes`${minutes}`;}
+    let time=`${hours}:${minutes}`;
+    return `${date.day()} ${date.time()}`;
+  }
+
+function handleSubmit (event){
+  event.preventDefault();
+  let apiKey = "4a7c01390293f156a41b247c2f0f0679";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(displayWeather);
+}
+function updateCity(event){
+  setCity(event.target.value);
+}
+
+ 
   return (
     <div className="App">
       <div className="container">
         <div className="weather-app">
-          <form className="search-form" id="search-form">
+          <form className="search-form" id="search-form" onSubmit={handleSubmit}>
             <div className="row">
               <div className="col-9">
                 <input
@@ -24,7 +63,7 @@ function App() {
                   placeholder="Type a city.."
                   autofocus="on"
                   id="city-input"
-                  className="form-control shadow-sm"
+                  className="form-control shadow-sm" onChange={updateCity}
                 />
               </div>
               <div className="col-3">
@@ -36,19 +75,19 @@ function App() {
               </div>
             </div>
           </form>
-          <h1 id="city">{weatherData.city}</h1>
+          <h1 id="city">{city}</h1>
           <ul>
-            <li id="date"> {weatherData.date}</li>
-            <li>{weatherData.description}</li>
+            <li id="date"> {formatDate(response.data.dt*1000)}</li>
+            <li>{weather.description}</li>
           </ul>
 
         <div class="row">
           <div class="col-6">
             <div class="clearfix">
-              <img src={weatherData.imgUrl} className="weather-icon" />
+              <img src={weather.imgUrl} className="weather-icon" />
               <div class="float-left">
                 <span className="temperature" id="temperature">
-                  {weatherData.temperature}
+                  {Math.round(weather.temperature)} 
                 </span>
                 <span class="units">
                   <a href="#" id="celsius-link">
@@ -64,7 +103,7 @@ function App() {
           </div>
           <div className="col-6">
             <ul>
-              <li>Precipitation:{weatherData.precipitation}% </li>
+              <li>Precipitation:{weather.precipitation}% </li>
               <li>Humidity:{weatherData.humidity}</li>
               <li>Wind: {weatherData.wind} km/h</li>
             </ul>
@@ -96,7 +135,7 @@ function App() {
     </div>
         </div>
   );
-}
+  }
       
 
 export default App;
